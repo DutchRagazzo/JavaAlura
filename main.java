64d8184032d33569ddf92e30611972a06e19a087
Gabriel Holanda <139644915+DutchRagazzo@users.noreply.github.com>
2025-04-17 21:06:59 -0300

    Atualizar o main.java

--- main.java
+++ main.java
@@ -1,7 +1,8 @@
-
 public class Main {
+    int idade;
+    String nome="Biel";
     public static void main(String[] args) {
-        System.out.println("Alô Mundo!");
+        System.out.println("Olá :" + nome);
     }
 
 }
